3c811073201ee7e4f113d40d37a1704c573f1cc0
Aditi Chak <aditichak@hotmail.com>
2017-03-19 09:53:27 -0700

    added chron job code

--- tweetparser/parser.py
+++ tweetparser/parser.py
@@ -2,6 +2,7 @@ import time
 import tweepy
 import re
 import HTMLParser
+import schedule
 
 consumer_key = 'AX99l75lGAiEXKewGv6UbjnzP'
 consumer_secret = 'UJtQXT02ieWvAEPy3kk7iqYMkL6Vom7zfIdTJZlyvMkSqegvPw'
@@ -9,7 +10,7 @@ access_token = '727254860375052289-dImeSLNMLOs4pDxnqypV3hytZL4rF49'
 access_secret = 'khWVlagGWmJ6S5F3rjt9wLJDTXBzZm4Hr4TqRdbzMwcIQ'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 html_parser = HTMLParser.HTMLParser()
 
 
@@ -20,13 +21,11 @@ def tweetText(d, count):
     for company in d:
         tweets = []
         public_tweets = tweepy.Cursor(api.search,
-                           q=company, include_entities=False, lang="en", since="2017-03-13", until="2017-03-18",
-                                      result_type="popular").items(count)
+                           q=company, include_entities=False, lang="en", since="2017-03-13", until="2017-03-18").items(count)
         data[company] = tweets
 
         while True:
             try:
-                tweet = public_tweets.next()
                 # Insert into db - potential for large data set for historical analysis
                 for tweet in public_tweets:
                     t = tweet.text
@@ -46,5 +45,10 @@ def tweetText(d, count):
 d = ["apple", "facebook", "exxon", "nvidia", "netflix", "adobe"]
 tweetText(d, 5)
 
+# schedule.every(3).days.at("01:00").do(tweetText(d,5000))
 
+# save to db?
 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
