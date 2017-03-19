bf648c2d3d929a1da642cb79a23ec418dd0bcea1
wes <wes.berry1@gmail.com>
2017-03-19 12:25:29 -0700

    Mock.

--- sentiment_twitter/sentiment_api.py
+++ sentiment_twitter/sentiment_api.py
@@ -11,7 +11,7 @@ def get_sentiment_average(text_array):
 def get_sentiment_score(messages):
     # data = str(messages)
     # the below string is an edited version of messages, but as a string.  This still fails on the API call...
-    data = "{'data':[{'text':'Exxon says a technical glitch means some Tillerson alias emails may be lost  '},{'text': u'Sec\'y of State Tillerson while at Exxon would use email alias Wayne Tracker to discuss climate change, per NY AG\n\n'},{'text': u'He has probably never worked this hard in his entire life. Imagine his shock and depression as he realizes he could\u2026 '}]}"
+    data = "{'data':[{'text':'Exxon sucks'}, {'text':'Exxon is okay'}]}"
     base_sentiment_url = "http://www.sentiment140.com/api/bulkClassifyJson"
     r = requests.post(base_sentiment_url, data=data)
     sum = 0
@@ -20,4 +20,6 @@ def get_sentiment_score(messages):
         sum = sum + entry['polarity']
         count += 1
     return sum/count
+
+print get_sentiment_score({})
 
