b389b50a74b027d7dd90836945d5b8c93cf90b75
Chris Semiao <csemiao@gmail.com>
2017-03-18 20:29:46 -0700

    price scraper up

--- historical-finance.py
+++ historical-finance.py
@@ -1,3 +1,33 @@
 from yahoo_finance import Share
-facebook = Share('FB')
-print(facebook.get_historical('2012-04-25', '2015-03-10'))
+import datetime
+
+import collections
+
+from dateutil.relativedelta import relativedelta
+
+"""
+Call with a ticket symbol to get an array of closing prices for the last three years, relative to the date which the
+function is called.  The returned array consists of pricepoint tuples, which can be accessed with .date and .price
+fields
+"""
+def getThreeYearClosingPrices(ticker):
+    facebook = Share(ticker)
+    data = facebook.get_historical((datetime.datetime.now() - relativedelta(years=3)).strftime("%Y-%m-%d"),
+                                   datetime.datetime.now().strftime("%Y-%m-%d"))
+
+    PricePoint = collections.namedtuple('PricePoint', 'date price')
+    price_data = []
+
+    for day in data:
+        pp = PricePoint(day['Date'], round(float(day['Close']), 2))
+        price_data.append(pp)
+
+    price_data.reverse()
+    return price_data
+
+
+print getThreeYearClosingPrices('FB')
+
+
+
+
